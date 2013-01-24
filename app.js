8d4d1d0624a88bc31163df42c47ebb67357d2186
Kalle Kainu <kkainu@gmail.com>
2013-01-24 23:41:03 +0200

    handle multiple images

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ var Bacon = require('baconjs').Bacon
 var app = require('http').createServer().listen(1337)
 var io = require('socket.io').listen(app)
 
-var imageUrlRegExp = /(https?:\/\/.*\.(?:png|jpg))/i
+var imageUrlRegExp = /(f|ht)tps?:\/\/.+?(jpg|png)(\s|$)/gi
 
 var client = new irc.Client('localhost', 'pixie', { channels: ['#pixie'] })
 
 var images = Bacon.fromEventTarget(client, 'message', function(from, to, message) { return message })
-					.filter( function(message) {
+					.filter(function(message) {
 						return imageUrlRegExp.test(message)	
+					})
+					.map(function(message) {
+						return message.match(imageUrlRegExp)
 					})
 
 io.sockets.on('connection', function (socket) {
-	images.onValue(function(value) { 
-		socket.emit('images', { url: value })
+	images.onValue(function(images) {
+		images.forEach(function(image) {
+		    socket.emit('images', { url: image })	
+		})
 	})
 })
