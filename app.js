4a722e6e00e10517952188f3c35bdbfecabf3b3f
Kalle Kainu <kkainu@gmail.com>
2013-01-24 23:01:16 +0200

    filter only messages with images

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var Bacon = require('baconjs').Bacon
 var app = require('http').createServer().listen(1337)
 var io = require('socket.io').listen(app)
 
-var client = new irc.Client('localhost', 'pixie', { channels: ['#pixie'] });
+var imageUrlRegExp = /(https?:\/\/.*\.(?:png|jpg))/i
 
-var messages = Bacon.fromEventTarget(client, 'message', function(from, to, message) { return message; });
+var client = new irc.Client('localhost', 'pixie', { channels: ['#pixie'] })
+
+var images = Bacon.fromEventTarget(client, 'message', function(from, to, message) { return message })
+					.filter( function(message) {
+						return imageUrlRegExp.test(message)	
+					})
 
 io.sockets.on('connection', function (socket) {
-	messages.onValue(function(value) { 
-		socket.emit('messages', { message: value });
-	});
-});
+	images.onValue(function(value) { 
+		socket.emit('images', { url: value })
+	})
+})
